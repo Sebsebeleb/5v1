48dafdce1f36d564dcadba4b7ffa9612e95432a3
Sebastian Vråle <seb@terminator.net>
2015-05-22 19:00:38 +0200

    Health and boss counter ui display

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class TurnManager : MonoBehaviour
 {
 
-    private int BossCounter = 80;
+    public int BossCounter = 80;
 
 
     public void UseTurn()
---
+++ Assets/Scripts/View/InfoDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Mime;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// Displays general info about the game, like player HP, boss coutner etc.
+/// </summary>
+public class InfoDisplay : MonoBehaviour
+{
+    public Text PlayerHealthText;
+    public Text BossCounterText;
+
+    private Damagable _playerHealth;
+    private TurnManager _turnManager;
+
+    void Awake()
+    {
+        _playerHealth = GameObject.FindWithTag("Player").GetComponent<Damagable>();
+        _turnManager = GameObject.FindWithTag("GM").GetComponent<TurnManager>();
+    }
+
+    void Update()
+    {
+        PlayerHealthText.text = _playerHealth.CurrentHealth.ToString() + "/" + _playerHealth.MaxHealth.ToString();
+        BossCounterText.text = _turnManager.BossCounter.ToString();
+    }
+}
