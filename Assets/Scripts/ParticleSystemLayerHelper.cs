827a8e44ef6e2c55af0a9a97420573c6637151b0
Sebastian Vråle <Seb@terminator.net>
2015-09-13 11:03:31 +0200

    Shop screen

---
+++ Assets/Scripts/ParticleSystemLayerHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [ExecuteInEditMode]
+    class ParticleSystemLayerHelper : MonoBehaviour 
+    {
+        void Start()
+        {
+            ParticleSystem ps = GetComponent<ParticleSystem>();
+            ps.GetComponent<Renderer>().sortingLayerName = "Particles";
+        }
+    }
+}
