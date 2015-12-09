6e6579e3486422372af2a026ce8dc1614d897511
Sebsebeleb <seb@terminator.net>
2015-12-09 14:48:21 +0100

    Targeting system improvments, and some other misc UI stuff

--- Assets/Scripts/View/EnemyCustomButtonBehaviour.cs
+++ Assets/Scripts/View/EnemyCustomButtonBehaviour.cs
@@ -1,47 +1,79 @@
-using System;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class EnemyCustomButtonBehaviour : MonoBehaviour, IPointerClickHandler
+public class EnemyCustomButtonBehaviour : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
 {
-	public GameObject InspectorPanel;
-	private InspectorPanelBehaviour _inspector;
+    public GameObject InspectorPanel;
+    private InspectorPanelBehaviour _inspector;
 
-	private View.GridButtonBehaviour _gridButton;
+    private View.GridButtonBehaviour _gridButton;
 
-	// Used to tell the player we want to target us
-	private PlayerTargeting _playerTargeting;
+    // Used to tell the player we want to target us
+    private PlayerTargeting _playerTargeting;
 
-	void Awake(){
-		_gridButton = GetComponent<View.GridButtonBehaviour>();
-		_inspector = InspectorPanel.GetComponent<InspectorPanelBehaviour>();
-		_playerTargeting = GameObject.FindWithTag("Player").GetComponent<PlayerTargeting>();
-	}
+    private bool isHeld;
 
-	public void OnPointerClick(PointerEventData eventData){
-		// Rightclick -> Inspect
-		if (eventData.button == PointerEventData.InputButton.Right){
-			InspectEnemy();
-		}
+    private float holdTime;
 
-		/*if (eventData.clickCount > 1){
-			InspectEnemy();
-		}*/
+    void Awake(){
+        _gridButton = GetComponent<View.GridButtonBehaviour>();
+        _inspector = InspectorPanel.GetComponent<InspectorPanelBehaviour>();
+        _playerTargeting = GameObject.FindWithTag("Player").GetComponent<PlayerTargeting>();
+    }
 
-		// Otherwise, target it
-		else{
-			TargetEnemy();
-		}
-	}
+    public void OnPointerClick(PointerEventData eventData){
 
-	private void InspectEnemy(){
-		Actor enemy = GridManager.TileMap.GetAt(_gridButton.x, _gridButton.y);
-		InspectorPanel.SetActive (true);
+        // Rightclick -> Inspect
+        if (eventData.button == PointerEventData.InputButton.Right){
+            InspectEnemy();
+        }
 
-		_inspector.InspectActor (enemy);
-	}
-	private void TargetEnemy(){
-		_playerTargeting.TargetGrid(_gridButton.x, _gridButton.y);
-	}
+
+        /*if (eventData.clickCount > 1){
+            InspectEnemy();
+        }*/
+
+        // Otherwise, target it
+        else{
+            TargetEnemy();
+        }
+    }
+
+    void Update()
+    {
+        if (this.isHeld)
+        {
+            this.holdTime += Time.deltaTime;
+        }
+        else
+        {
+            this.holdTime = 0f;
+        }
+
+        if (this.holdTime > 0.4f)
+        {
+            this.InspectEnemy();
+        }
+
+    }
+
+    private void InspectEnemy(){
+        Actor enemy = GridManager.TileMap.GetAt(_gridButton.x, _gridButton.y);
+        InspectorPanel.SetActive (true);
+
+        _inspector.InspectActor (enemy);
+    }
+    private void TargetEnemy(){
+        _playerTargeting.TargetGrid(_gridButton.x, _gridButton.y);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        this.isHeld = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        this.isHeld = false;
+    }
 }
